1c620447ca182b128acd21fe6979c5a010854232
Perla Yates <pianistps@yahoo.com>
2016-11-28 20:45:40 +0000

    deleted default welcome erb, edited spec

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -4,10 +4,10 @@ def app
   ApplicationController
 end
 
-describe ApplicationController do
-  it "responds with a welcome message" do
-    get '/'
-    expect(last_response.status).to eq(200)
-    expect(last_response.body).to include("Welcome to the Sinatra Template!")
-  end
-end
+# describe ApplicationController do
+#   it "responds with a welcome message" do
+#     get '/'
+#     expect(last_response.status).to eq(200)
+#     expect(last_response.body).to include("Welcome to the Sinatra Template!")
+#   end
+# end
