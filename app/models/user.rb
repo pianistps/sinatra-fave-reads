6ff0d42cef1256f43a2150fa2750810c68eb6e8b
Perla Yates <pianistps@yahoo.com>
2016-11-29 18:18:18 +0000

    added slug methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+  has_secure_password
  has_many :books
  has_many :authors, through: :books
+
+ def slug
+   self.username.gsub(" ","-")
+ end
+
+ def self.find_by_slug(arg)
+   new_arg = arg.gsub("-"," ")
+   User.find_by(username: new_arg )
+ end
 end
