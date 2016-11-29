a9114a17fb4ba19b8ffdf35bba4712d63348a978
Perla Yates <pianistps@yahoo.com>
2016-11-29 18:17:51 +0000

    edited user spec for name instead of username

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,15 +2,15 @@ require 'spec_helper'
 
 describe 'User' do
   before do
-    @user = User.create(:username => "test 123", :email => "test123@aol.com", :password => "test")
+    @user = User.create(:name => "test 123", :email => "test123@aol.com", :password => "test")
   end
-  it 'can slug the username' do
+  it 'can slug the name' do
     expect(@user.slug).to eq("test-123")
   end
 
   it 'can find a user based on the slug' do
     slug = @user.slug
-    expect(User.find_by_slug(slug).username).to eq("test 123")
+    expect(User.find_by_slug(slug).name).to eq("test 123")
   end
 
   it 'has a secure password' do
