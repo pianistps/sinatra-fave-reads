91be42e2cc96b1b6a7e2e909118d9402b35a8233
Perla Yates <pianistps@yahoo.com>
2016-11-29 21:02:47 +0000

    added the signup form and post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
   get '/users/new' do
     erb :'/users/signup'
   end
+
+  post '/signup' do
+    @user = User.create(params)
+    redirect '/'
+  end
 end
