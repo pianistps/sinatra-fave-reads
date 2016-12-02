5d89d4adb51e36fe3333f8fedcab4c7a16c459d7
Perla Yates <pianistps@yahoo.com>
2016-12-02 18:14:53 +0000

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,16 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
+  helpers do
+    def current_user
+      @user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def is_logged_in?
+      !!current_user
+    end
+  end
+
   get "/" do
     erb :index
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,9 @@ require './config/environment'
 class UsersController < ApplicationController
 
   get '/signup' do
+
     erb :'/users/new'
+    
   end
 
   post '/signup' do
