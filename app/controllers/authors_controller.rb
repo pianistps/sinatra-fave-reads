a3c743debfa70ffee4156b5046c0842d8faac5d8
Perla Yates <pianistps@yahoo.com>
2016-12-02 17:49:25 +0000

    added environment

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,3 +1,5 @@
+require './config/environment'
+
 class AuthorsController < ApplicationController
 
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,5 @@
+require './config/environment'
+
 class BooksController < ApplicationController
 
 end
