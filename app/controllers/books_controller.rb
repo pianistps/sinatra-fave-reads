210f90b57bd4d7f5acf9a37b7e92c45bf923091c
Perla Yates <pianistps@yahoo.com>
2016-12-06 00:23:03 +0000

    added a find by for user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,6 +4,7 @@ class BooksController < ApplicationController
 
   get '/books' do
     if is_logged_in?
+      @user = User.find_by_id(current_user.id)
       erb :'/books/index'
     else
       redirect '/'
