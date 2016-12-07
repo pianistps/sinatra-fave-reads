4ea5752989544fbae39211d21e86f3b37bdd6353
Perla Yates <pianistps@yahoo.com>
2016-12-07 22:38:26 +0000

    added a necessary user and book id

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ActiveRecord::Base
  belongs_to :author
  validates_presence_of :title
  validates_presence_of :summary
+ validates :user_id, presence: true
+ validates :author_id, presence: true
 end
