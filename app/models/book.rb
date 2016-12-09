29cf4f16ddb79c275e0014207992bf81055fbfca
Perla Yates <pianistps@yahoo.com>
2016-12-09 19:17:28 +0000

    cleaned up validation of data code

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,5 @@
 class Book < ActiveRecord::Base
  belongs_to :user
  belongs_to :author
- validates_presence_of :title
- validates_presence_of :summary
- validates :user_id, presence: true
- validates :author_id, presence: true
+ validates :user_id, :title, :summary, :author_id, presence: true
 end
