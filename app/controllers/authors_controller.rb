764143e7572bae53c46e9e69843e81e1f489351a
Perla Yates <pianistps@yahoo.com>
2016-12-09 19:40:52 +0000

    added action for authors index and added an vidual authors page, also added some style choices to view

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,7 +4,13 @@ class AuthorsController < ApplicationController
 
   get '/authors' do
     if is_logged_in?
+      @user = User.find_by_id(current_user.id)
+      @authors = @user.authors
       erb :'/authors/index'
     end
+  end
+
+  get '/authors/:id' do
+    @author = Author.find_by_id(params[:id])
   end
 end
